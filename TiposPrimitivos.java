f67cdf51c9c6dacf0cfb5feebb6d7b5fa171e4fc
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-05-04 11:09:58 -0300

    Create TiposPrimitivos.java

---
+++ TiposPrimitivos.java
@@ -0,0 +1,14 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+      String noma= "Ronaldo";
+      String endereco = "Av.Brasil número:58 primavera";
+      String telefone = "(98)98607-8013";
+      System.out.println("Nome " + noma + "Endereço: " + endereco + "Telefone: " + telefone  );
+
+      double salario = 2.500;
+      String sexo = "M";
+      int idade = 32;
+      String estadoCivil= "Casado";
+      System.err.println( "O trabalhador "+ noma + "do sexo " + sexo + " idade " + idade + ", estado civil "+ estadoCivil + " salario "+ salario + " encontra-se empregado neste estabelecimento.");
+    }
+}
