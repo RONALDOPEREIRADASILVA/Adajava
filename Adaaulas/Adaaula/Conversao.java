f18d6ae558b5c8cc8c46131d7a5376be347f001e
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-05-28 15:39:57 -0300

    Create Conversao.java

---
+++ Adaaulas/Adaaula/Conversao.java
@@ -0,0 +1,12 @@
+package Adaaulas.Adaaula;
+
+public class Conversao {
+    void main(){
+        //Variável
+        String n1 ="5";
+        int n2 = 5;
+
+        //Soma
+        System.out.println(Integer.parseInt(n1) + n2);
+    }
+}
