f82148f16c8c1737cb711dc6f3198b814bbce13e
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-08-18 10:27:59 -0300

    Update LacoPara.java

--- Adaaulas/Adaaula/LacoPara.java
+++ Adaaulas/Adaaula/LacoPara.java
@@ -3,7 +3,7 @@ package Adaaulas.Adaaula;
 public class LacoPara {
     void main(){
         //Laço de repetição
-        for(int indice = 1; indice <= 20; indice++){
+        for(int indice = 0; indice <= 20; indice++){
             System.out.println(indice);
             if (indice== 20 ) {
                 System.out.println("você chegou ao final da contagem");
