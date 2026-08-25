feff71f533692a370dbf5f81d55b7006dd0c3faa
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-08-25 10:11:11 -0300

    Create aula06EstruturaDeRepeticao02.java

---
+++ aula06EstruturaDeRepeticao02.java
@@ -0,0 +1,15 @@
+
+/*
+Imprima todos os números de 0 a 100000
+*/
+
+
+public class aula06EstruturaDeRepeticao02 {
+    public static void main(String[] args){
+        for(int i=0; i <= 100000; i++){
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }   
+}
